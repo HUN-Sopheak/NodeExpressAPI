d04c246b99fa67cf586847520b69295373a1e4ab
Sopheak Hun <sopheakhun030@gmail.com>
2024-01-16 17:13:13 +0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+// Attempt to connect to the database
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        throw err;
+    }
+
+    console.log('Connected to the database successfully');
+});
+
+// Handle errors during the connection
+connection.on('error', (err) => {
+    console.error('Database connection error:', err);
+    // You might want to handle errors, close connections, or take other appropriate actions here
+});
+
+module.exports = connection;
