c28d8e582826b6a8e1038d96399f361f7d869c05
Sopheak Hun <sopheakhun030@gmail.com>
2024-01-19 17:19:32 +0700

    update

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,7 @@
 const userController = require('../controllers/user.controller');
 const user = (router, authenticateToken) => {
 	router.get('/user/get', authenticateToken, userController.lists);
+	router.get('/user/create', authenticateToken, userController.lists);
 };
 
 module.exports = user;
